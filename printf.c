f6ddd3e6767821b510c2ec4b1b3428622bbaa310
Emmanuel <nwaforemmanuel005@gmail.com>
2023-01-25 13:11:25 +0100

    correct main logic

--- printf.c
+++ printf.c
@@ -8,21 +8,27 @@ int _printf(const char *format, ...)
 {
 	int i, n;
 	va_list args;
+	int flag;
 
 	i = 0;
 	n = 0;
+	flag = 0;
+	if (format == NULL)
+		return (n);
 	va_start(args, format);
 	while (format[i])
 	{
-		if ((format[i] != '%' && format[i - 1] != '%') ||
-		(format[i - 2] == '%' && format[i - 1] == '%' && format[i] != '%'))
+		if (format[i] == '%' && !flag)
+		{
+			flag = !flag;
+		} else if (!flag)
 		{
 			_putchar(format[i]);
 			n++;
-		}
-		else if (format[i - 1] == '%')
+		} else
 		{
 			get_specifier(format[i])(&args, &n);
+			flag = !flag;
 		}
 		i++;
 	}
