dc537a783657e08ea1cbff265cce54227208d0e4
Emmanuel Nwafor <nwaforemmanuel005@gmail.com>
2023-01-25 17:27:34 +0100

    move function - betty

--- specifier.c
+++ specifier.c
@@ -79,23 +79,3 @@ void spec_i(va_list *args, int *n)
 {
 	spec_d(args, n);
 }
-/**
- * spec_b - unsigned int converted to binary
- * @args: variable list
- * @n: pointer to variable
- * Return: nothing
- */
-void spec_b(va_list *args, int *n)
-{
-	int number;
-
-	number = va_arg(*args, int);
-	if (number == 0)
-	{
-		_putchar('0');
-		return;
-	}
-	if (number < 0)
-		number = number * (-1);
-	print_binary(number, n);
-}
